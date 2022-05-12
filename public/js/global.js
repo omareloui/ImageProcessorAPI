0e12a479bce4ee0a76b95d0ae77fc6ede1db9206
Omar Hussein <omarelwy@hotmail.com>
2022-05-12 19:24:06 +0200

    ♻️ Some refactoring
    - Remove fethear icons.
    - Render the error page only if it's 404 or 500. Else the the plain text.
    - Add min max to the inputs for image operations.

--- public/js/global.js
+++ public/js/global.js
@@ -1,7 +1,3 @@
-import "feather-icons";
-
-feather.replace();
-
 console.log(
   "%chttps://omareloui.com %c2022",
   "color: #f5f5f5; background: #121212; padding: 5px 10px; font-size: 16px; margin: 10px 0; border-radius: 4px 0 0 4px",
--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,9 +1,12 @@
 import type { ErrorRequestHandler } from "express";
 
 export const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
-  res.status(err.statusCode || 500).render("error", {
-    statusCode: err.statusCode,
-    message: err.message,
-    statusMessage: err.statusMessage,
-  });
+  res.status(err.statusCode || 500);
+  if (res.statusCode === 500 || res.statusCode === 404)
+    res.render("error", {
+      statusCode: err.statusCode,
+      message: err.message,
+      statusMessage: err.statusMessage,
+    });
+  else res.send(err.message);
 };
