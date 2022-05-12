698716b63e82dcc0654234ece359634d2ba49aa6
Omar Hussein <omarelwy@hotmail.com>
2022-05-12 23:50:24 +0200

    ✅ Make sure /:image response with 404 if can't find the image

--- src/__tests__/routes/views.spec.ts
+++ src/__tests__/routes/views.spec.ts
@@ -1,5 +1,13 @@
+import supertest from "supertest";
+import { app } from "../..";
+
+const request = supertest(app);
+
 describe("Views", () => {
   describe("/:image", () => {
-    it("should go to 404 on visiting an image that doesn't exist");
+    it("should go to 404 on visiting an image that doesn't exist", async () => {
+      const res = await request.get("/any-image-that-doest-exist.png");
+      expect(res.statusCode).toBe(404);
+    });
   });
 });
